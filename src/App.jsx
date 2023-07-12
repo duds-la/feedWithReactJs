535c3955f826d8f8644292c4a0a0e73ae031717d
Eduardo Lazzaretti <duadsal@gmail.com>
2023-07-12 20:02:00 -0300

    Aditing format dates with date-fns

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,44 @@ import styles from "./App.module.css";
 
 import './global.css';
 
+const posts = [
+  {
+    id: 1,
+    author:{
+      avatarUrl: 'https://github.com/eduardola.png',
+      name:'Eduardo Lazzaretti',
+      role: 'Desenvolvedor'
+    },
+    content:[
+      {type: 'paragraph', content:'Fala galeraa 👋',},
 
+      {type: 'paragraph', content:'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return,' },
+      {type: 'paragraph', content:'evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+
+      {type: 'link', content:'jane.design/doctorcare'},
+
+    ],
+    publishedAt: new Date('2023-07-12 18:30:30'),
+  },
+  {
+    id: 2,
+    author:{
+      avatarUrl: 'https://github.com/maikbrito.png',
+      name:'Mayk Brito',
+      role: 'Desenvolvedor'
+    },
+    content:[
+      {type: 'paragraph', content:'Fala galeraa 👋',},
+
+      {type: 'paragraph', content:'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return,' },
+      {type: 'paragraph', content:'evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+
+      {type: 'link', content:'jane.design/doctorcare'},
+
+    ],
+    publishedAt: new Date('2023-07-10 18:30:30'),
+  },
+];
 
 export function App() {
   return (
@@ -17,15 +54,14 @@ export function App() {
       <div className={styles.wrapper}>
           <Sidebar />
           <main>
-            <Post
-              author="tigas"
-              content="loren"
-            />
-
-            <Post
-              author="tigas"
-              content="loren"
-            />
+            {posts.map(post =>{
+              return (
+              <Post 
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />)
+            })}
           </main>
       </div>
     </div>
